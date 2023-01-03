7d05ed152178a55eaaf0c6c4bdee5583a945e7df
Rick Huff <rhuff2@yahoo.com>
2023-01-03 06:01:56 -0600

    Daily commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ɵNOT_FOUND_CHECK_ONLY_ELEMENT_INJECTOR } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { HomeComponent } from './components/home/home.component';
+import { JobApplicationComponent } from './components/job-application/job-application.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'contact', component: ContactMeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'job', component: JobApplicationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { JobApplicationComponent } from './components/job-application/job-application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    HomeComponent,
+    ContactMeComponent,
+    JobApplicationComponent
   ],
   imports: [
     BrowserModule,
